9a093f9e6870a8667498ef05839ee6778a3463c7
Elijah Oyekunle <eloyekunle@gmail.com>
2018-10-29 14:39:25 +0100

    added 'Problem' struct

--- 1/1/main.go
+++ 1/1/main.go
@@ -6,6 +6,11 @@ import (
 	"path/filepath"
 )
 
+type Problem struct {
+	Question string
+	Answer   string
+}
+
 func main() {
 	problemsFile, _ := filepath.Abs("../problems.csv")
 	reader := bufio.NewReader(os.Stdin)
