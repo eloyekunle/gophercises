57a45bd51c8259121678081a5b8da9984fa2a707
Elijah Oyekunle <eloyekunle@gmail.com>
2018-10-29 15:52:02 +0100

    successfully asking all questions

--- 1/1/main.go
+++ 1/1/main.go
@@ -3,17 +3,35 @@ package main
 import (
 	"bufio"
 	"encoding/csv"
+	"fmt"
+	"io"
+	"log"
 	"os"
+	"strings"
 )
-
-type Problem struct {
-	Question string
-	Answer   string
-}
 
 func main() {
 	problemsFile, _ := os.Open("../problems.csv")
 	reader := csv.NewReader(bufio.NewReader(problemsFile))
-
+	var totalProblems, correctAnswers int
 	inputReader := bufio.NewReader(os.Stdin)
+
+	for {
+		line, error := reader.Read()
+		if error == io.EOF {
+			break
+		} else if error != nil {
+			log.Fatal(error)
+		}
+
+		totalProblems++
+		fmt.Println(line[0])
+		ans, _ := inputReader.ReadString('\n')
+		ans = strings.TrimSuffix(ans, "\n")
+
+		if ans == line[1] {
+			correctAnswers++
+		}
+	}
+	fmt.Println(correctAnswers, totalProblems)
 }
