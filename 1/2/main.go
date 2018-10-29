ec3951072cfc55e38cd14f7cb6067a60bf917ab8
Elijah Oyekunle <eloyekunle@gmail.com>
2018-10-29 18:10:58 +0100

    added ticker

--- 1/2/main.go
+++ 1/2/main.go
@@ -14,7 +14,7 @@ import (
 
 func main() {
 	csvFilePtr := flag.String("csv", "../problems.csv", "A CSV file in the format of 'question,answer'")
-	limitPtr := flag.Int("limit", 30, "The time limit for the quiz in seconds.")
+	limitPtr := flag.Int("limit", 2, "The time limit for the quiz in seconds.")
 	flag.Parse()
 
 	csvFile, _ := os.Open(*csvFilePtr)
@@ -22,27 +22,30 @@ func main() {
 	var totalProblems, correctAnswers int
 	inputReader := bufio.NewReader(os.Stdin)
 
-	go func() {
-		for {
-			line, error := reader.Read()
-			if error == io.EOF {
-				break
-			} else if error != nil {
-				log.Fatal(error)
-			}
-
-			totalProblems++
-			fmt.Printf("Problem #%d: %s = ", totalProblems, line[0])
-
-			ticker := time.NewTicker(time.Second * time.Duration(*limitPtr))
-
-			ans, _ := inputReader.ReadString('\n')
-			ans = strings.TrimSpace(ans)
-
-			if ans == line[1] {
-				correctAnswers++
-			}
+	for {
+		line, error := reader.Read()
+		if error == io.EOF {
+			break
+		} else if error != nil {
+			log.Fatal(error)
 		}
-	}()
+
+		totalProblems++
+		fmt.Printf("Problem #%d: %s = ", totalProblems, line[0])
+
+		ticker := time.NewTicker(time.Second * time.Duration(*limitPtr))
+
+		select {
+		case <-ticker.C:
+			break
+		}
+
+		ans, _ := inputReader.ReadString('\n')
+		ans = strings.TrimSpace(ans)
+
+		if ans == line[1] {
+			correctAnswers++
+		}
+	}
 	fmt.Printf("Answered %d correctly out of %d.\n", correctAnswers, totalProblems)
 }
